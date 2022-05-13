c77424e325a2af651d197568bc69db62fe6ed374
LiprikON2 <44316968+LiprikON2@users.noreply.github.com>
2022-05-14 02:59:22 +0300

    init

--- src/scripts/game.ts
+++ src/scripts/game.ts
@@ -1,30 +1,30 @@
-import 'phaser'
-import MainScene from './scenes/mainScene'
-import PreloadScene from './scenes/preloadScene'
+import "phaser";
+import MainScene from "./scenes/mainScene";
+import PreloadScene from "./scenes/preloadScene";
 
-const DEFAULT_WIDTH = 1280
-const DEFAULT_HEIGHT = 720
+const DEFAULT_WIDTH = 1280;
+const DEFAULT_HEIGHT = 720;
 
 const config = {
-  type: Phaser.AUTO,
-  backgroundColor: '#ffffff',
-  scale: {
-    parent: 'phaser-game',
-    mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH,
-    width: DEFAULT_WIDTH,
-    height: DEFAULT_HEIGHT
-  },
-  scene: [PreloadScene, MainScene],
-  physics: {
-    default: 'arcade',
-    arcade: {
-      debug: false,
-      gravity: { y: 400 }
-    }
-  }
-}
+    type: Phaser.AUTO,
+    backgroundColor: "#1d252c",
+    scale: {
+        parent: "phaser-game",
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: DEFAULT_WIDTH,
+        height: DEFAULT_HEIGHT,
+    },
+    scene: [PreloadScene, MainScene],
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: false,
+            gravity: { y: 4000 },
+        },
+    },
+};
 
-window.addEventListener('load', () => {
-  const game = new Phaser.Game(config)
-})
+window.addEventListener("load", () => {
+    const game = new Phaser.Game(config);
+});
