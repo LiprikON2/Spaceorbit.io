3cb693fb2bfa7fd66d887549f556b0c37057fdba
LiprikON2 <44316968+LiprikON2@users.noreply.github.com>
2022-05-21 13:12:05 +0300

    refactor

---
+++ src/assets/maps/map.d.ts
@@ -0,0 +1,23 @@
+// DUMMY
+export interface mapAtlas {
+    frames: {
+        map: {
+            frame: {
+                x: number;
+                y: number;
+                w: number;
+                h: number;
+            };
+        };
+    };
+    meta: {
+        image: string;
+        format: string;
+        size: {
+            w: number;
+            h: number;
+        };
+        scale: number;
+        bgColor: string;
+    };
+}
