27a427e443a0fbe3b003d18b52053cdb8854fcc9
LiprikON2 <44316968+LiprikON2@users.noreply.github.com>
2023-06-28 16:05:58 +0300

    chore: update scripts

---
+++ client/scripts/apk-build.sh
@@ -0,0 +1,33 @@
+cd ..
+
+if [[ -n $(git status -s) ]]; then
+    echo "Commit changes before proceeding"
+    exit 1
+fi
+
+rm -rf ./android/app/build/outputs/apk/debug > /dev/null
+rm -rf ./android/app/build/outputs/apk/release > /dev/null
+rm -rf ./android/app/release > /dev/null
+
+read -s -p "Enter keystore password: " keystorepass
+
+npm run prod-build
+
+npx cap build android \
+    --keystorepass $keystorepass \
+    --keystorepath "../releases/keystore.jks" \
+    --keystorealias main \
+    --keystorealiaspass $keystorepass \
+    --androidreleasetype APK
+
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | xargs echo -n)
+
+cp ./android/app/build/outputs/apk/debug/app-debug.apk "./releases/spaceorbit-$PACKAGE_VERSION-debug.apk" 2> /dev/null
+cp ./android/app/build/outputs/apk/release/app-release-unsigned.apk "./releases/spaceorbit-$PACKAGE_VERSION-release-unsigned.apk" 2> /dev/null
+cp ./android/app/build/outputs/apk/release/app-release-signed.apk "./releases/spaceorbit-$PACKAGE_VERSION-release.apk" 2> /dev/null
+cp ./android/app/release/app-release.apk "./releases/spaceorbit-$PACKAGE_VERSION-release.apk" 2> /dev/null
---
+++ client/scripts/release.sh
@@ -0,0 +1,61 @@
+cd ..
+
+if [[ -n $(git status -s) ]]; then
+    echo "Commit changes before proceeding"
+    exit 1
+fi
+
+if ! command -v gh >/dev/null 2>&1; then
+    echo "Couldn't find github CLI: 'gh'"
+    exit 1
+fi
+
+if ! gh auth status >/dev/null 2>&1; then
+    gh auth login
+fi
+
+case "$(uname -sr)" in
+
+    Linux*)
+        os='Linux'
+        gum=./gum
+    ;;
+
+    CYGWIN*|MINGW*|MINGW32*|MSYS*)
+        os='Windows'
+        gum=./gum.exe
+
+    ;;
+
+    *)
+        echo "Couldn't determine th OS, defaulting to Linux"
+        os='Linux'
+        gum=./gum
+    ;;
+esac
+
+PACKAGE_VERSION=$(npm pkg get version --workspaces=false | tr -d \")
+
+splitByDot () {
+   awk -F'.' '{ for( i=1; i<=NF; i++ ) print $i }' <<< $1
+}
+
+version=($(splitByDot $PACKAGE_VERSION))
+major=${version[0]}
+minor=${version[1]}
+patch=${version[2]}
+
+option1="$major.$minor.$patch"
+option2="$major.$minor.$((patch+1))"
+option3="$major.$((minor+1)).0"
+option4="$((major+1)).0.0"
+
+echo "Choose new package version (current $PACKAGE_VERSION)"
+choosen_version=$(echo -e "$option1\n$option2\n$option3\n$option4" | $gum choose)
+echo $choosen_version
+
+npm pkg set version=$choosen_version
+
+apks=$(find ./releases -maxdepth 1 -name "*$choosen_version*")
+
+gh release create $choosen_version $apks
