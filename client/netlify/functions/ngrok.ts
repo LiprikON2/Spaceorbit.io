e90c2c120cece50ac9816d47efc4b38998627623
LiprikON2 <44316968+LiprikON2@users.noreply.github.com>
2023-07-31 18:09:04 +0300

    inc

--- client/netlify/functions/ngrok.ts
+++ client/netlify/functions/ngrok.ts
@@ -1,11 +1,10 @@
 exports.handler = async (event, context) => {
     console.log("event", event);
     console.log("context", context);
-    //Securely access environment variables here
-    console.log("process.env.ngrok_api", process.env.ngrok_api);
+    console.log("process.env.ngrok_api", process.env.NGROK_API);
 
     return {
         statusCode: 200,
-        body: JSON.stringify({ api: process.env.ngrok_api }),
+        body: JSON.stringify({ api: `hmm: ${process.env.NGROK_API}` }),
     };
 };
